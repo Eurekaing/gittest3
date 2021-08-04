123ff439536a5cabfd4fa6d6b13041c897fc6628
zhouchenzhen <zhouchenzhen@163.com>
2021-08-04 18:31:59 +0800

    first commit

---
+++ src/main/java/com/zcz/gitTest.java
@@ -0,0 +1,11 @@
+package com.zcz;
+
+/**
+ * @author shkstart
+ * @creat 2021-08-04-18:30
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
